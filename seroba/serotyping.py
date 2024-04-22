56181273af3921ffdb09f7bf04362df2c951dd3e
Oliver-Lorenz-dev <oliverlorenz.business@gmail.com>
2024-04-22 16:32:34 +0100

    mutations for 24

--- seroba/serotyping.py
+++ seroba/serotyping.py
@@ -481,11 +481,11 @@ class Serotyping:
                     if 'HET' in line:
                         flag = 'contamination'
             with open(self.prefix+'/pred.tsv', 'a') as fobj:
-                if '24' in self.sero:
-                    fobj.write(self.prefix+'\tserogroup 24\t'+flag+'\n')
                 # 7D CPS may not be representative, so do not fully resolve to 7D
-                elif '07D' in self.sero:
+                if '07D' in self.sero:
                     fobj.write(self.prefix + '\t07B/07D\t' + flag + '\n')
+                elif '24B' in self.sero or '24F' in self.sero:
+                    fobj.write(self.prefix + '\t24B/24F\t' + flag + '\n')
                 else:
                     fobj.write(self.prefix+'\t'+self.sero+'\t'+flag+'\n')
             shutil.rmtree(os.path.join(self.prefix,'ref'))
